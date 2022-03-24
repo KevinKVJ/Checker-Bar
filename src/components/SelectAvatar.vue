485382849ea779fdb5e62c37047a601bdaa5fd2a
KevinKVJ <jiakaiwen5216@gmail.com>
2022-03-23 23:43:45 -0600

    Some changes of setting and extensions recommended and add a jsconfig.json

--- src/components/SelectAvatar.vue
+++ src/components/SelectAvatar.vue
@@ -1,5 +1,4 @@
 <template>
-    <!-- <div class="selectSection"> -->
         <div class="imageSection">
             <SvgIcon name="Avatar1" class="avatarImages"></SvgIcon>
             <SvgIcon name="Avatar2" class="avatarImages"></SvgIcon>
@@ -7,14 +6,7 @@
             <SvgIcon name="Avatar4" class="avatarImages"></SvgIcon>
             <SvgIcon name="Avatar5" class="avatarImages"></SvgIcon>
             <SvgIcon name="Avatar6" class="avatarImages"></SvgIcon>
-           <!-- <img src="@/assets/icons/Avatar1.svg" alt="Avatar1" class="avatarImages">
-           <img src="@/assets/icons/Avatar2.svg" alt="Avatar2" class="avatarImages">
-           <img src="@/assets/icons/Avatar3.svg" alt="Avatar3" class="avatarImages">
-           <img src="@/assets/icons/Avatar4.svg" alt="Avatar4" class="avatarImages">
-           <img src="@/assets/icons/Avatar5.svg" alt="Avatar5" class="avatarImages">
-           <img src="@/assets/icons/Avatar6.svg" alt="Avatar6" class="avatarImages"> -->
         </div>
-    <!-- </div> -->
 </template>
 
 <style>
