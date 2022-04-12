fffb290b07bd10533c41eee6064c17ee76367919
HiddenBoxI <44384527+HiddenBoxI@users.noreply.github.com>
2022-04-12 05:57:36 -0600

    Version02

--- src/components/Checker/CheckerBoard3.vue
+++ src/components/Checker/CheckerBoard3.vue
@@ -339,14 +339,20 @@ let theOnlyChess = -1;
 
 // import {moveChess} from './logic.js'
 const moveChess = ([oriRow, oriCol], [newRow, newCol],RealChessboard,activeChess) => {
+
+
+
   // activeChess.value = null;
 
   if (RealChessboard[oriRow][oriCol].chessObj != null) {
+
+
     moveAgain = false;
 
     if (!canGo) {
 
       if (RealChessboard[oriRow][oriCol].chessObj[1] === 1 || RealChessboard[oriRow][oriCol].chessObj[1] === 0) {
+
 
 
         RealChessboard[newRow][newCol].chessObj = RealChessboard[oriRow][oriCol].chessObj;
@@ -482,6 +488,8 @@ const moveByGridCoord = newCoord => {
 
 // import {letActive} from './logic.js'
 // import {secondMove} from './logic.js'
+
+
 
 function letActive(newAC,RealChessboard) {
 
@@ -1151,10 +1159,22 @@ function secondMove(newAC,RealChessboard) {
 
 }
 
+function timeCount(){
+  if (newR!==null){
+    console.log("if");
+    clearInterval();
+
+  }else {
+    canGo=!canGo;
+    console.log("else");
+  }
+}
 
 
 
 watch(activeChess, (newAC, OriAC) => {
+
+  //setInterval(timeCount,5000,newAC);
 
   if (newAC !== null) {
     const {chessType, csIndex, row, column} = newAC;
@@ -1184,6 +1204,7 @@ watch(activeChess, (newAC, OriAC) => {
     }
 
 
+
     //moveAgain = false;
 
 
@@ -1192,7 +1213,11 @@ watch(activeChess, (newAC, OriAC) => {
 
 
 
+
+
   }
+
+
 });
 
 
